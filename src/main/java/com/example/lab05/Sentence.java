4ced902f1ed8f8e2e35d9e93b86b08afed7c49ed
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-09-13 10:52:12 +0700

    finish

---
+++ src/main/java/com/example/lab05/Sentence.java
@@ -0,0 +1,13 @@
+package com.example.lab05;
+
+import java.util.ArrayList;
+
+public class Sentence {
+    public ArrayList<String> badSentences;
+    public ArrayList<String> goodSentences;
+
+    public Sentence() {
+        this.badSentences = new ArrayList();
+        this.goodSentences = new ArrayList();
+    }
+}
